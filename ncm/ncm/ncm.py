c238584bc3ad7448d339ca9590961408bd30c245
Kim Streich <kim@infusedinsight.com>
2024-10-04 15:36:34 -0600

    Prevent duplication of GET parameters

--- ncm/ncm/ncm.py
+++ ncm/ncm/ncm.py
@@ -51,6 +51,7 @@ Tips:
 from requests import Session
 from requests.adapters import HTTPAdapter
 from http import HTTPStatus
+from urllib.parse import urlencode
 from urllib3.util.retry import Retry
 from datetime import datetime, timedelta
 import sys
@@ -220,8 +221,11 @@ class NcmClientv2(BaseNcmClient):
                         chunk_str = ','.join(map(str, chunk))
                         params.update({key: chunk_str})
                         url = get_url
+                        if params is not None:
+                            query_string = urlencode(params)
+                            url = f'{url}?{query_string}'
                         while url and (len(results) < limit):
-                            ncm = self.session.get(url, params=params)
+                            ncm = self.session.get(url)
                             ncm.raise_for_status()
                             if not (200 <= ncm.status_code < 300):
                                 break
@@ -234,8 +238,11 @@ class NcmClientv2(BaseNcmClient):
 
         if __in_keys == 0:
             url = get_url
+            if params is not None:
+                query_string = urlencode(params)
+                url = f'{url}?{query_string}'
             while url and (len(results) < limit):
-                ncm = self.session.get(url, params=params)
+                ncm = self.session.get(url)
                 ncm.raise_for_status()
                 if not (200 <= ncm.status_code < 300):
                     break
